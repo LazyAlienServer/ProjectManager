38712a5f03f03bb6d831e9f370b10a9a73f7cec5
DIOJOIO <diojoio41@163.com>
2024-08-29 01:51:03 +0800

    ProjectManager

---
+++ src/client/java/top/lazyalienserver/projectmanager/client/ProjectmanagerClient.java
@@ -0,0 +1,10 @@
+package top.lazyalienserver.projectmanager.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectmanagerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/top/lazyalienserver/projectmanager/Projectmanager.java
@@ -0,0 +1,10 @@
+package top.lazyalienserver.projectmanager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Projectmanager implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
